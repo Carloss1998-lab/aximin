ee335588e1bbe4eb5d0bbfa0cdf42244a934d858
Carloss1998-lab <calebcarloss@gmail.com>
2021-05-30 17:57:01 +0200

    ReadUpdate2

--- src/app/pages/notifications/notifications.ts
+++ src/app/pages/notifications/notifications.ts
@@ -44,16 +44,32 @@ export class NotificationsPage extends BasePage {
   
       this.notifications = await this.notificationsService.load();
       
+      //console.log("ICI1");
 
       //this.notificationsUpdate = await this.notificationsService.loadOne("83BkJIOF5F");
+      //console.log(this.notificationsUpdate);
 
-      // await this.notificationsUpdate.markAsSeenNotif();
-      console.log("ICI");
+      //await this.notificationsUpdate.markAsSeenNotif();
+      //console.log("ICI");
 
      function IsCurrentUser(element) {
       return (element["users"]["0"].id == Parse.User.current().id); 
    }
       this.notifications = this.notifications.filter(IsCurrentUser);
+
+// Change read = true
+
+      //console.log("Code");
+      //this.notificationsUpdate = await this.notificationsService.loadOne(element.id);
+      //await this.notificationsUpdate.markAsSeenNotif();
+      //console.log("marche")
+
+      for (var element of this.notifications) {
+        if (element["read"]==false) {
+          this.notificationsUpdate = await this.notificationsService.loadOne(element.id);
+          await this.notificationsUpdate.markAsSeenNotif();
+        }
+      }
 
       if (this.notifications.length) {
         this.showContentView();
